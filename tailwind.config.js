71f463c2ff16990d4ba319fdc7b5a4ab0092b923
Jhon Sebastian Navas Chafuelan <jsnavas@cenicana.org>
2023-02-01 15:43:15 -0500

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./resources/views/index.blade.php",
+    "./resources/views/vendor/pagination/tailwind.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
